d2792b2fe05ba2318e3f2e1bc9b2789f258e55d8
waresujay5 <waresujay5@gmail.com>
2025-02-04 13:03:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 #(©)Codexbotz
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1002484977793
+pyrogram.utils.MIN_CHANNEL_ID = -1002327021905
 from aiohttp import web
 from plugins import web_server
 
